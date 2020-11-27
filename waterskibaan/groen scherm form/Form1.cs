f2937fc9074e4f29fe27756570fd360656358a25
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2020-11-27 09:32:56 +0100

    Add files via upload

---
+++ waterskibaan/groen scherm form/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace groenschermfrom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = @"C:\Users\jaime\OneDrive\Afbeeldingen\Schermopnamen\yes.png";
+            string firstName = "john";
+            string lastName = "de Boer";
+            nameOutput.Text = "welkom: " + firstName + " " + lastName; }
+
+        private void pictureBox1_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
