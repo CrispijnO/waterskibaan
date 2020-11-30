930fe495b930630deaf82d33c306bd71c570e36b
Thiimo <thimosietsma@gmail.com>
2020-11-30 22:06:33 +0100

    .

--- waterskibaan/Program.cs
+++ waterskibaan/Program.cs
@@ -16,7 +16,8 @@ namespace waterskibaan
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new Form1());
+            APIHelper.InitializeClient();
         }
     }
 }
