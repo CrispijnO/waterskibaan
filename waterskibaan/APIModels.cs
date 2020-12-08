bb0f63dd3d30553b7ed9f582ea13879b6a57d50d
Thiimo <thimosietsma@gmail.com>
2020-12-08 10:52:00 +0100

    Added API Models & a gitignore file.

---
+++ waterskibaan/APIModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace waterskibaan
+{
+    class boekingen
+    {
+        public int id { get; set; }
+        public int klant_Id { get; set; }
+        public string datum { get; set; }
+        public bool online_boeking { get; set; }
+    }
+
+    class contacten
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public string naam { get; set; }
+    }
+
+    class klanten
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public string displaynaam { get; set; }
+        public string naam { get; set; }
+        public string adres { get; set; }
+        public string plaats { get; set; }
+    }
+
+    class startmomenten
+    {
+        public int id { get; set; }
+        public int startmomentgroep_id { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
