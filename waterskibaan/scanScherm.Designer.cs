86f891324c16b2d1ac125df57ea4f21598904ce0
supr3mion <230690@edu.rocfriesepoort.nl>
2020-12-10 15:42:41 +0100

    dagrecreant waarschuwwing toegevoegd

--- waterskibaan/scanScherm.Designer.cs
+++ waterskibaan/scanScherm.Designer.cs
@@ -46,6 +46,7 @@ namespace groenschermfrom
             this.nameOutput = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.errorCheck = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.roodPanel.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -58,10 +59,9 @@ namespace groenschermfrom
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(300, 475);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(225, 386);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(317, 46);
+            this.button1.Size = new System.Drawing.Size(238, 37);
             this.button1.TabIndex = 4;
             this.button1.Text = "Scan sherm";
             this.button1.UseVisualStyleBackColor = true;
@@ -69,15 +69,14 @@ namespace groenschermfrom
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.roodPanel);
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.roundPictureBox1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(-1, 15);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Location = new System.Drawing.Point(-1, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(921, 453);
+            this.panel1.Size = new System.Drawing.Size(691, 368);
             this.panel1.TabIndex = 1;
             // 
             // roodPanel
@@ -85,10 +84,9 @@ namespace groenschermfrom
             this.roodPanel.BackColor = System.Drawing.Color.DarkRed;
             this.roodPanel.Controls.Add(this.groupBox2);
             this.roodPanel.Controls.Add(this.label2);
-            this.roodPanel.Location = new System.Drawing.Point(0, -11);
-            this.roodPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.roodPanel.Location = new System.Drawing.Point(3, 3);
             this.roodPanel.Name = "roodPanel";
-            this.roodPanel.Size = new System.Drawing.Size(913, 466);
+            this.roodPanel.Size = new System.Drawing.Size(685, 379);
             this.roodPanel.TabIndex = 12;
             // 
             // groupBox2
@@ -96,31 +94,28 @@ namespace groenschermfrom
             this.groupBox2.BackColor = System.Drawing.Color.White;
             this.groupBox2.Controls.Add(this.pictureBox3);
             this.groupBox2.Controls.Add(this.richTextBox2);
-            this.groupBox2.Location = new System.Drawing.Point(108, 6);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Location = new System.Drawing.Point(81, 5);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox2.Size = new System.Drawing.Size(591, 199);
+            this.groupBox2.Size = new System.Drawing.Size(443, 162);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             // 
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.SystemColors.Menu;
-            this.pictureBox3.Location = new System.Drawing.Point(380, 23);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Location = new System.Drawing.Point(285, 19);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(133, 149);
+            this.pictureBox3.Size = new System.Drawing.Size(100, 121);
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             // 
             // richTextBox2
             // 
             this.richTextBox2.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.richTextBox2.Location = new System.Drawing.Point(41, 20);
+            this.richTextBox2.Location = new System.Drawing.Point(31, 16);
             this.richTextBox2.Margin = new System.Windows.Forms.Padding(0);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(284, 158);
+            this.richTextBox2.Size = new System.Drawing.Size(214, 129);
             this.richTextBox2.TabIndex = 1;
             this.richTextBox2.Text = "";
             // 
@@ -129,29 +124,27 @@ namespace groenschermfrom
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 129.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.label2.Location = new System.Drawing.Point(268, 209);
+            this.label2.Location = new System.Drawing.Point(201, 170);
             this.label2.Margin = new System.Windows.Forms.Padding(0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(249, 246);
+            this.label2.Size = new System.Drawing.Size(198, 195);
             this.label2.TabIndex = 4;
             this.label2.Text = "X";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(612, 139);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(459, 113);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(265, 22);
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
             this.textBox1.TabIndex = 6;
             // 
             // roundPictureBox1
             // 
             this.roundPictureBox1.BackColor = System.Drawing.Color.DarkGray;
             this.roundPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("roundPictureBox1.Image")));
-            this.roundPictureBox1.Location = new System.Drawing.Point(-63, 155);
-            this.roundPictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.roundPictureBox1.Location = new System.Drawing.Point(-47, 126);
             this.roundPictureBox1.Name = "roundPictureBox1";
-            this.roundPictureBox1.Size = new System.Drawing.Size(356, 341);
+            this.roundPictureBox1.Size = new System.Drawing.Size(267, 277);
             this.roundPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.roundPictureBox1.TabIndex = 5;
             this.roundPictureBox1.TabStop = false;
@@ -161,11 +154,11 @@ namespace groenschermfrom
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.SlateBlue;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 42F);
-            this.label1.Location = new System.Drawing.Point(-43, 21);
+            this.label1.Location = new System.Drawing.Point(-32, 17);
             this.label1.Margin = new System.Windows.Forms.Padding(0);
             this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(80, 6, 80, 6);
-            this.label1.Size = new System.Drawing.Size(958, 91);
+            this.label1.Padding = new System.Windows.Forms.Padding(60, 5, 60, 5);
+            this.label1.Size = new System.Drawing.Size(754, 74);
             this.label1.TabIndex = 3;
             this.label1.Text = "Please Scan Down Here";
             // 
@@ -173,77 +166,85 @@ namespace groenschermfrom
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(367, 139);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(275, 113);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(192, 279);
+            this.pictureBox1.Size = new System.Drawing.Size(144, 227);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // GroenPanel
             // 
+            this.GroenPanel.Controls.Add(this.roodPanel);
             this.GroenPanel.Controls.Add(this.labelTime);
             this.GroenPanel.Controls.Add(this.nameOutput);
             this.GroenPanel.Controls.Add(this.pictureBox2);
             this.GroenPanel.Location = new System.Drawing.Point(-1, 0);
-            this.GroenPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.GroenPanel.Name = "GroenPanel";
-            this.GroenPanel.Size = new System.Drawing.Size(917, 468);
+            this.GroenPanel.Size = new System.Drawing.Size(688, 380);
             this.GroenPanel.TabIndex = 5;
             // 
             // labelTime
             // 
             this.labelTime.BackColor = System.Drawing.Color.Lime;
-            this.labelTime.Location = new System.Drawing.Point(212, 139);
-            this.labelTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTime.Location = new System.Drawing.Point(159, 113);
             this.labelTime.Name = "labelTime";
-            this.labelTime.Size = new System.Drawing.Size(445, 37);
+            this.labelTime.Size = new System.Drawing.Size(334, 30);
             this.labelTime.TabIndex = 3;
             // 
             // nameOutput
             // 
             this.nameOutput.BackColor = System.Drawing.Color.Snow;
             this.nameOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameOutput.Location = new System.Drawing.Point(171, 60);
+            this.nameOutput.Location = new System.Drawing.Point(128, 49);
+            this.nameOutput.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.nameOutput.Name = "nameOutput";
-            this.nameOutput.Size = new System.Drawing.Size(529, 41);
+            this.nameOutput.Size = new System.Drawing.Size(397, 33);
             this.nameOutput.TabIndex = 2;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(-8, 4);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(-3, 3);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(921, 513);
+            this.pictureBox2.Size = new System.Drawing.Size(691, 417);
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
             // errorCheck
             // 
             this.errorCheck.AutoSize = true;
-            this.errorCheck.Location = new System.Drawing.Point(659, 490);
+            this.errorCheck.Location = new System.Drawing.Point(494, 398);
+            this.errorCheck.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.errorCheck.Name = "errorCheck";
-            this.errorCheck.Size = new System.Drawing.Size(176, 21);
+            this.errorCheck.Size = new System.Drawing.Size(133, 17);
             this.errorCheck.TabIndex = 6;
             this.errorCheck.Text = "error? (checked is yes)";
             this.errorCheck.UseVisualStyleBackColor = true;
             this.errorCheck.CheckedChanged += new System.EventHandler(this.errorButton_CheckedChanged_1);
             // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label3.Location = new System.Drawing.Point(430, 302);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(247, 54);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "als u een dagrecreant bent, \r\ndan hoeft u niet te scannen";
+            // 
             // scanScherm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(917, 526);
+            this.ClientSize = new System.Drawing.Size(688, 427);
             this.Controls.Add(this.errorCheck);
             this.Controls.Add(this.GroenPanel);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "scanScherm";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
@@ -279,6 +280,7 @@ namespace groenschermfrom
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.CheckBox errorCheck;
+        private System.Windows.Forms.Label label3;
     }
 }
 
