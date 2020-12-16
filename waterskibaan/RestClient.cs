0170f66cd042918b37ba746fa30d1d7431a8d2e0
Thiimo <thimosietsma@gmail.com>
2020-12-16 09:56:46 +0100

    New API Key

--- waterskibaan/RestClient.cs
+++ waterskibaan/RestClient.cs
@@ -34,7 +34,7 @@ namespace waterskibaan
 
             request.Method = httpMethod.ToString();
             request.Host = "demo.recras.nl";
-            request.Headers.Add("Authorization", "Bearer Gh70CBYp5JIK56UqJgToHPxgtLl2Zsnm");
+            request.Headers.Add("Authorization", "Bearer af-Qy9i7gwYVW2ISHTPnpX2BS4rVK7ET");
             HttpWebResponse response = null;
 
             try
