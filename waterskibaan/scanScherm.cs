20db01ff155d910b7a3347fc577b8ea37a8a0ac0
Thiimo <thimosietsma@gmail.com>
2020-12-17 14:42:43 +0100

    Last functional changes!

--- waterskibaan/scanScherm.cs
+++ waterskibaan/scanScherm.cs
@@ -87,10 +87,10 @@ namespace groenschermfrom
         {
             //SetTimer();
             //IntervalClick();
-            IntervalClick();
+            /*IntervalClick();
             await Task.Delay(3000);
-            IntervalClick();
-            testTHIS(8424);
+            IntervalClick();*/
+            testTHIS(8418);
         }
 
         public void getReaders()
@@ -154,13 +154,16 @@ namespace groenschermfrom
                 switch (hexadecimalResult)
                 {
                     case "04-BA-B7-82-E0-60-80":
-                        braceletCode.braceletCode = 51;
+                        braceletCode.braceletCode = 56;
+                        // rood
                         break;
                     case "04-31-BF-5A-91-5B-80":
-                        braceletCode.braceletCode = 110;
+                        braceletCode.braceletCode = 8418;
+                        // groen
                         break;
                     case "04-17-BD-5A-91-5B-80":
-                        braceletCode.braceletCode = 101;
+                        braceletCode.braceletCode = 8422;
+                        // groen (tot 3 uur)
                         break;
                     default:
                        // textBox1.Text = "Unknown code";
@@ -197,7 +200,7 @@ namespace groenschermfrom
             int index = 0;
             bool found = false;
             DateTime DateNow = DateTime.Now;
-            responseBoeking.ForEach(boeking =>
+            responseBoeking.ForEach(async boeking =>
             {
                 for (int i = 0; i < boeking.boekingsregels.Count - 1; i++)
                 {
@@ -211,30 +214,48 @@ namespace groenschermfrom
                         break;
                     }
                 }
-                if (found)
+                Console.WriteLine("index: " + index);
+                DateTime eerstVolgendeBoeking = boeking.boekingsregels[0].begin;
+                if (!found)
                 {
+                    Console.WriteLine("This is getting executed.");
                     for (int i = 0; i < boeking.boekingsregels.Count - 1; i++)
                     {
                         var boekingsregelW = boeking.boekingsregels[i];
                         // looping over elke boekingsregel en kijken welke boeking het eerst aan de beurt is.
                         // ook checken of het niet al geweest is.
-                        DateTime eerstVolgendeBoeking = DateTime.Now;
-                        if (boekingsregelW.begin > eerstVolgendeBoeking)
-                        {
 
+                        Console.WriteLine("test.");
+                        Console.WriteLine(boekingsregelW.begin + " - " + eerstVolgendeBoeking);
+                        if (eerstVolgendeBoeking > boekingsregelW.begin && DateTime.Now < boekingsregelW.begin)
+                        {
+                            Console.WriteLine("this is getting set.");
+                            eerstVolgendeBoeking = boekingsregelW.begin;
+                            index = i;
                         }
                     }
                 }
                 var boekingsregel = boeking.boekingsregels[index];
                 TimeSpan t = timeLeft(boekingsregel.begin, boekingsregel.eind);
-                Console.WriteLine("Hours: " + t.Hours);
-                if (t.Minutes < 1)
+                if (!found)
+                // time for start is in eerstVolgendeBoeking en de index is in index.
                 {
+                    richTextBox2.Text = $"Hallo {responseKlant.displaynaam},\n\nU heeft op did moment geen boeking.\nUw eerst volgende boeking is: {eerstVolgendeBoeking}";
                     GroenPanel.Visible = true;
                     roodPanel.Visible = true;
+                    await Task.Delay(5000);
+                    roodPanel.Visible = false;
+                    GroenPanel.Visible = false;
+                    richTextBox2.Text = "";
                 } else
                 {
+                    nameOutput.Text = $"Hallo {responseKlant.displaynaam}!";
+                    labelTime.Text = $"U heeft nog {formatTime(t)} tegaan!";
                     GroenPanel.Visible = true;
+                    await Task.Delay(5000);
+                    GroenPanel.Visible = false;
+                    labelTime.Text = "";
+                    nameOutput.Text = "";
                 }
                 string formattedTimeString = formatTime(t);
                 Console.WriteLine(formattedTimeString);
@@ -243,7 +264,7 @@ namespace groenschermfrom
         public TimeSpan timeLeft(DateTime start, DateTime end)
         {
             DateTime now = DateTime.Now;
-            Console.WriteLine("[NOW | END] " + now + " | " + end);
+            //Console.WriteLine("[NOW | END] " + now + " | " + end);
             TimeSpan result = end - now;
             return result;
         }
@@ -259,43 +280,43 @@ namespace groenschermfrom
             {
                 if (time.Days == 1)
                 {
-                    formattedTime += string.Format("{0} Dag, ", time.Days);
+                    formattedTime += string.Format("{0} dag, ", time.Days);
                 }
                 else
                 {
-                    formattedTime += string.Format("{0} Dagen, ", time.Days);
+                    formattedTime += string.Format("{0} dagen, ", time.Days);
                 }
             }
             if (days || hours)
             {
                 if (time.Hours == 1)
                 {
-                    formattedTime += string.Format("{0} Uur, ", time.Hours);
+                    formattedTime += string.Format("{0} uur, ", time.Hours);
                 }
                 else
                 {
-                    formattedTime += string.Format("{0} Uren, ", time.Hours);
+                    formattedTime += string.Format("{0} uren, ", time.Hours);
                 }
             }
             if (days || hours || minutes)
             {
                 if (time.Minutes == 1)
                 {
-                    formattedTime += string.Format("{0} Minuut, ", time.Minutes);
+                    formattedTime += string.Format("{0} minuut, ", time.Minutes);
                 } else
                 {
-                    formattedTime += string.Format("{0} Minuten, ", time.Minutes);
+                    formattedTime += string.Format("{0} minuten, ", time.Minutes);
                 }
             }
             if (days || hours || minutes || seconds)
             {
                 if (time.Seconds == 1)
                 {
-                    formattedTime += string.Format("{0} Seconde", time.Seconds);
+                    formattedTime += string.Format("{0} seconde", time.Seconds);
                 }
                 else
                 {
-                    formattedTime += string.Format("{0} Secondes", time.Seconds);
+                    formattedTime += string.Format("{0} secondes", time.Seconds);
                 }
             }
             return formattedTime;
